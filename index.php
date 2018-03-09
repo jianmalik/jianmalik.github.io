e9986fa638f2ac9bd520246e8948a9e6a4c91fbd
jianmlkhdyt <37133301+jianmlkhdyt@users.noreply.github.com>
2018-03-09 08:38:49 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title>Jianmlkhdyt</title>
+		<link rel="stylesheet" type="text/css" href="css/Jian.css">
+
+</head>
+<body>
+<p align="center"><marquee scrollamount="4">
+			<font color="red"><font face = "Chiller">
+			Jianmalikhidayat@gmail.com<br><font size="6">
+					</font></marquee>
+							</p>
+
+	<h1><font color="white"><font face = "Chiller"><center><font size="7"> Welcome Web Jian.malik </h1></font>
+		<p align="center">
+			<font color="white"><font face = "Chiller"><font size="6">
+				Praktik Pemograman Jaringan<br>
+					</font>
+							</p>
+
+<div class="vid-container">
+  <video id="Video1" class="bgvid back" autoplay="false" muted="muted" preload="auto" loop>
+      <source src="video/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+  </video>
+    <div class="inner-container">
+		<video id="Video2" class="bgvid inner" autoplay="false" muted="muted" preload="auto" loop>
+			<source src="video/milky-way-river-1280hd.mp4.mp4" type="video/mp4">
+ 
+ </video>
+ 
+    <div class="box center">
+    <p align="Center">
+	<table> 
+	<tr>
+	<td> Nama</td>
+	<td> Jian Malik Hidayat</td>
+</tr>
+	<tr>
+	<td> Kelas</td>
+	<td> 4CB</td>
+</tr>
+
+<tr>
+ <td>Jurusan</td>
+ <td> Teknik Komputer</td>
+</tr>
+
+<tr>
+ <td>
+Politeknik Negeri Sriwijaya 
+</td>
+</tr>
+
+</table>
+</p>
+	<img src="images/jian.JPG" alt="Icon" width="220" height="320">
+
+    </div>
+  </div>
+  
+</div>
+
+<table height="40" width="100%" bgcolor="gray"><font face= "Chiller">
+	<tr align="left">
+		<td align="Center"><b><i><font size="5"><font face= "Chiller">&copy;  2018</i></b></td>
+			<td align="right"><font size="5" color="black"><font face= "Chiller">  Jian Malik Hidayat</font></td>
+	</tr>
+</body>
+</html>
